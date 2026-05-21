cd6766b3cddd94f60ff0bb8d38aa6d02be0ef276
Nifemi <adekogbe08@gmail.com>
2026-05-21 23:04:07 +0100

    added PopularDishes and AboutServices components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/src/components/sections/hero";
+import PopularDishes from "@/src/components/sections/popularDishes";
+import AboutServices from "@/src/components/sections/aboutService";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <PopularDishes />
+      <AboutServices />
     </main>
   );
 }
